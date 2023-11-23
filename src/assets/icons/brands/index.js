e10a8e1bf5714b0835b6d230e75b0f913e1b837a
216-coding-ninja <tumelo.mahlangu@216.co.za>
2023-11-23 21:07:10 +0200

    updated assets

---
+++ src/assets/icons/brands/index.js
@@ -0,0 +1,21 @@
+import AfricanBank from '@/assets/icons/brands/africanBank.png';
+import AutoZone from '@/assets/icons/brands/autozone.png';
+import Eswatini from '@/assets/icons/brands/eswatini.png';
+import FNB from '@/assets/icons/brands/fnb.png'
+import Hollard from '@/assets/icons/brands/hollard.png';
+import Polokwane from '@/assets/icons/brands/polokwane.png';
+import RMA from '@/assets/icons/brands/RMA.png';
+import Sena from '@/assets/icons/brands/sena.png';
+import Vodacom from '@/assets/icons/brands/vodacom.png';
+
+export{
+    AfricanBank,
+    AutoZone,
+    Eswatini,
+    FNB,
+    Hollard,
+    Polokwane,
+    RMA,
+    Sena,
+    Vodacom
+}
