65109c6e63cd500c761d9d7cb546ab5c09fa9611
216-coding-ninja <tumelo.mahlangu@216.co.za>
2023-12-05 13:01:04 +0200

    added companyInfo page

---
+++ src/common/components/Footer/component/CompanyInfo.jsx
@@ -0,0 +1,97 @@
+import {
+    styled,
+    Box,
+    Typography
+} from '@mui/material';
+
+
+import 
+    ImportContactsIcon 
+from '@mui/icons-material/ImportContacts';
+
+import 
+    PhoneIcon 
+from '@mui/icons-material/Phone';
+
+import 
+    EmailIcon 
+from '@mui/icons-material/Email';
+
+import { 
+    Colors 
+} from '@/common/constants';
+
+const CompanyInfoContent = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column'
+}))
+
+const AddressContainer = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'flex-start',
+    gap: theme.spacing(1.2),
+    marginBottom: theme.spacing(1)
+}))
+
+const CompanyInfoText = styled(Typography)(({ theme }) => ({
+    fontSize: '13px',
+    color: Colors.White,
+    width: '200px',
+    marginBottom: theme.spacing(1)
+}))
+
+const CompanyTitle = styled(Typography)(({ theme }) => ({
+    fontSize: '20px',
+    color: Colors.White,
+    marginBottom: theme.spacing(4)
+}))
+
+const AddressIcon = styled(ImportContactsIcon)({
+    color: Colors.White,
+    width: '20px'
+})
+
+const PhoneNumberIcon = styled(PhoneIcon)({
+    color: Colors.White,
+    width: '20px'
+})
+
+const EmailAddressIcon = styled(EmailIcon)({
+    color: Colors.White,
+    width: '20px'
+})
+
+function CompanyInfo(){
+    return(
+        <CompanyInfoContent>
+            <CompanyTitle>
+                Have a Questions?
+            </CompanyTitle>
+            
+            <AddressContainer>
+                <AddressIcon />
+                <CompanyInfoText>
+                    50 Constantia Boulevard
+                    Quadrum Office Park, 
+                    Johannesburg, South Africa, 1709
+                </CompanyInfoText>
+            </AddressContainer>
+
+            <AddressContainer>
+                <PhoneNumberIcon />
+                <CompanyInfoText>
+                   011 534 8409
+                </CompanyInfoText>
+            </AddressContainer>
+
+            <AddressContainer>
+                <EmailAddressIcon />
+                <CompanyInfoText>
+                   info@216.co.za
+                </CompanyInfoText>
+            </AddressContainer>
+        </CompanyInfoContent>
+    )
+}
+
+export default CompanyInfo;
