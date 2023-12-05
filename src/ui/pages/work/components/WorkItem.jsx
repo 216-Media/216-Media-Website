3f847a7fe45fccfc6878e15955c92c069a59a260
216-coding-ninja <tumelo.mahlangu@216.co.za>
2023-12-06 00:48:54 +0200

    updated work components

---
+++ src/ui/pages/work/components/WorkItem.jsx
@@ -0,0 +1,142 @@
+import{
+    ROA_flags,
+    RMA_brand_car
+} from '@/assets/images';
+
+import 
+    CustomButton 
+from '@/common/components/CustomButton';
+
+import { 
+    Colors 
+} from '@/common/constants';
+
+import{
+    styled,
+    Container,
+    Typography,
+    Box,
+    useMediaQuery
+} from '@mui/material';
+
+import { 
+    Fragment 
+} from 'react';
+
+const WorkItemContainer = styled(Container)(({ theme }) => ({
+    display: 'flex',
+    justifyContent: 'center',
+    gap: theme.spacing(4),
+    alignItems: 'center',
+    paddingBlock: theme.spacing(4),
+    [theme.breakpoints.down('md')]: {
+        flexDirection: 'column'
+    }
+}))
+
+const WorkItemDetailsContainer = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: theme.spacing(2),
+
+}))
+
+const WorkItemProfile = styled('img')({
+    width: '400px'
+})
+
+const WorkTitleText = styled(Typography)(({ theme }) => ({
+    fontSize: '17px',
+    color: Colors.AZTEC,
+    letterSpacing: '2px',
+    
+}))
+
+const WorkItemClient = styled(Typography)(({ theme }) => ({
+    fontSize: '33px',
+    color: Colors.AZTEC,
+    width: '90%',
+    // [theme.breakpoints.down('md')]: {
+    //     width:
+    // }
+}))
+
+const WorkItemDescription = styled(Typography)(({theme}) => ({
+    fontSize: '17px',
+    color: Colors.ScaleGrey,
+    textAlign: 'left',
+    maxWidth: '450px',
+    paddingBottom: theme.spacing(2)
+}))
+
+function WorkItem(){
+
+    const isSmallerScreen = useMediaQuery((theme) => theme.breakpoints.down('md'));
+    const isLargerScreen = useMediaQuery((theme) => theme.breakpoints.up('lg')); 
+
+    const workItems = [
+        {
+            id: 1, 
+            workTitle: 'Brand Refresh', 
+            workImage: ROA_flags, 
+            client: 'Racehorse Owners Association (ROA)', 
+            description: 'Our team  aimed to better engage racehorse owners and enthusiasts, aligning ROA with current industry trends while preserving its esteemed heritage.'
+        },
+        {
+            id: 2,
+            workTitle: 'Brand Refresh',
+            workImage: ROA_flags,
+            client: 'Blank Page Production (BPP)',
+            description: "Our team dedicated extensive efforts towards revitalizing and modernizing BPP's visual identity and messaging elements"
+        },
+        {
+            id: 3,
+            workTitle: 'Interior Design',
+            workImage: ROA_flags,
+            client: 'FNB Stadium',
+            description: "216 Media collaborated with FNB Stadium to reimagine and revamp the interior spaces within their premises."
+        },
+        {
+            id: 4,
+            workTitle: 'Vehicle Brand',
+            workImage: RMA_brand_car,
+            client: 'Rand Mutual Assurance (RMA)',
+            description: "We spearheaded impactful vehicle branding initiatives, ensuring RMA's and their partners message reached diverse audiences."
+        }
+    ];
+
+    return(
+        <Fragment>
+            {workItems.map((work, index) => (
+                <WorkItemContainer
+                    key={index}
+                    style={{ 
+                        flexDirection: isSmallerScreen ? 'column' : index % 2 !== 0 ? 'row-reverse' : 'row',
+                    }}
+                >
+                    <WorkItemProfile 
+                        src={work.workImage}
+                        alt='profile-img'
+                    />
+                    <WorkItemDetailsContainer>
+                        <WorkTitleText>
+                            {work.workTitle}
+                        </WorkTitleText>
+                        <WorkItemClient>
+                            {work.client}
+                        </WorkItemClient>
+                        <WorkItemDescription
+                            variant='p'
+                        >
+                            {work.description}
+                        </WorkItemDescription>
+
+                        <CustomButton buttonText={'View Portfolio'}/>
+                    </WorkItemDetailsContainer>
+                </WorkItemContainer>
+            ))}
+        </Fragment>
+    )
+}
+
+export default WorkItem;
